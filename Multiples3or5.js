eefb9a3167a4d96e7c67cb5cad93a519bf62c34b
carobatata <aacostacarolina@gmail.com>
2021-12-28 16:26:33 +0100

    multiples3or5

---
+++ Multiples3or5.js
@@ -0,0 +1,26 @@
+// Multiples of 3 or 5
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+// 1° - encontrar los multiplos de 3 y 5 debajo de mil
+
+var multiples = function() {
+    var sumMultiples = 0;
+    num = 999
+    while(num > 0) {
+        if(num % 3 === 0 || num % 5 === 0) {
+            sumMultiples = sumMultiples + num;
+        }
+            num = num - 1;
+        }
+        return sumMultiples;
+    }
+
+// multiples() = 233168
+
+    // resultado = 233168
+
+
+// sumar todos esos resultados.
